7be52a66f418ded70c38b158eed57a1fa59e09a4
ashish60808 <ashish_kumar@ashishs-MacBook-Air.local>
2023-11-01 17:44:31 +0530

    Adding config file with P schema

--- src/main/java/com/springcore/Main.java
+++ src/main/java/com/springcore/Main.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
+        ApplicationContext context=new ClassPathXmlApplicationContext("config-pSchema.xml");
         Student student1=(Student) context.getBean("student1");
         System.out.println(student1);
         System.out.println(student1.getStudentAddress());
